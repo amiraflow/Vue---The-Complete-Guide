74c2e7f5d04281b112222d18786b232848abfd0a
amiraflow <amira.midzic@gmail.com>
2022-06-13 17:12:10 +0200

    Data binding and interpolation

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn Vue!',
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: 'Master Vue!',
             vueLink: 'https://vuejs.org/'
         };
     },
@@ -9,10 +10,10 @@ const app = Vue.createApp({
         outputGoal(){
             const randomNumber = Math.random();
             if(randomNumber < 0.5){
-                return 'Learn Vue';
+                return this.courseGoalA;
             }
             else{
-                return 'Master Vue';
+                return this.courseGoalB;
             }
         }
     }
