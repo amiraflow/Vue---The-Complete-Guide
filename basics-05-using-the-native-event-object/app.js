99fd1de950761e3ecec39dd497aa5ed0aa5d591a
amiraflow <amira.midzic@gmail.com>
2022-06-14 09:42:24 +0200

    Bad way of outputting varibles that depend on something

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput(){
+      this.name = '';
+    },
+    outputFullName(){
+      return this.name + ' ' + 'Schwarzmuller';
+    }
+  }
+});
+
+app.mount('#events');
