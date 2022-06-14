f78fde8db505a874a6c447bb2d7122057f8a6cde
amiraflow <amira.midzic@gmail.com>
2022-06-14 09:57:12 +0200

    Introducing watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,15 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
     };
+  },
+  watch: {
+    counter(value){
+      if(value > 50){
+        this.counter = 0;
+      }
+    }
   },
   computed: {
     fullname(){
-      if(this.name === ''){
+      if(this.name === '' || this.lastName === ''){
         return '';
       }
-      return this.name + ' ' + 'Schwarzmuller';      
+      return this.name + ' ' + this.lastName;      
     }
   },
   methods: {
