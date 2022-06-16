fda96f734083f1f89a6fbb4f98fcfb88982c8cf1
amiraflow <amira.midzic@gmail.com>
2022-06-16 11:54:38 +0200

    Simple component app

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+
+<li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone: </strong>{{friend.phone}}</li>
+        <li><strong>Email: </strong>{{friend.email}}</li>
+    </ul>
+</li>
+
+</template>
+
+<script>
+export default {
+    data(){
+    return {
+        detailsAreVisible: false,
+        friend: {
+            
+                id: 'amira',
+                name: 'Amira M',
+                phone: '0000',
+                email: 'amira.midzic@gmail.com'
+            },
+        };
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+}
+</script>
