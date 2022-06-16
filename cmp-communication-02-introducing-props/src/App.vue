f3e2a8661ed93e8a86f9bee49c3725d037f0bdb7
amiraflow <amira.midzic@gmail.com>
2022-06-16 12:13:44 +0200

    Props - parent child relationship

--- cmp-communication-02-introducing-props/src/App.vue
+++ cmp-communication-02-introducing-props/src/App.vue
@@ -1,56 +1,56 @@
 <template>
-<section>
-<header><h1>My friends</h1></header>
-<ul>
-   <friend-contact name="Manuel Lorenz" phone-number="000" email-address="manuel@mail.com"></friend-contact>
-   <friend-contact name="Manuel Lorenzo" phone-number="000" email-address="manuel@mail.com"></friend-contact>
-
-</ul>
-</section>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        name="Manuel Lorenz"
+        phone-number="01234 78992"
+        email-address="manuel@localhost.com"
+      ></friend-contact>
+      <friend-contact
+        name="Julie Jones"
+        phone-number="0987 65431"
+        email-address="julie@localhost.com"
+      ></friend-contact>
+    </ul>
+  </section>
 </template>
 
 <script>
-import FriendContact from './components/FriendContact.vue';
 export default {
-  components: { FriendContact },
-    data() {
-        return {
-            friends: [
-                {
-                    id: 'amira',
-                    name: 'Amira M',
-                    phone: '0000',
-                    email: 'amira.midzic@gmail.com'
-                },
-
-                {
-                    id: 'ajla',
-                    name: 'Ajla H',
-                    phone: '0000',
-                    email: 'ajla.h@gmail.com'
-                },
-            ]
-        };
-    },
-}
-
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
-
 * {
   box-sizing: border-box;
 }
-
 html {
-  font-family: 'Jost', sans-serif;
+  font-family: "Jost", sans-serif;
 }
-
 body {
   margin: 0;
 }
-
 header {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 3rem auto;
@@ -62,13 +62,11 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app ul {
   margin: 0;
   padding: 0;
   list-style: none;
 }
-
 #app li {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
@@ -78,14 +76,12 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app h2 {
   font-size: 2rem;
   border-bottom: 4px solid #ccc;
   color: #58004d;
   margin: 0 0 1rem 0;
 }
-
 #app button {
   font: inherit;
   cursor: pointer;
@@ -95,12 +91,10 @@ header {
   padding: 0.05rem 1rem;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
-
 #app button:hover,
 #app button:active {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
-
 </style>
--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,11 +1,11 @@
 <template>
 
 <li>
-    <h2>{{friend.name}}</h2>
+    <h2>{{name}}</h2>
     <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
     <ul v-if="detailsAreVisible">
-        <li><strong>Phone: </strong>{{friend.phone}}</li>
-        <li><strong>Email: </strong>{{friend.email}}</li>
+        <li><strong>Phone: </strong>{{phoneNumber}}</li>
+        <li><strong>Email: </strong>{{emailAddress}}</li>
     </ul>
 </li>
 
@@ -13,6 +13,11 @@
 
 <script>
 export default {
+    props: [
+        'name', 
+        'phoneNumber',
+        'emailAddress'
+    ],
     data(){
     return {
         detailsAreVisible: false,
