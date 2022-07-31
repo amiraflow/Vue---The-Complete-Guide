57c4e16591d1e47b5301c68c22c5827a87e7a19c
amiraflow <amira.midzic@gmail.com>
2022-07-31 12:19:28 +0200

    Adding a dialog

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,3 +1,36 @@
 <template>
-<h2>Manage Goals</h2>
+<div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref = "goal"/>
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+        Invalid input
+        <button @click="confirmError">OK</button>
+    </error-alert>
+</div>
+
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+  components: { ErrorAlert },
+    data(){
+        return {
+            inputIsInvalid: false
+        };
+    },
+    methods: {
+        setGoal(){
+            const enteredValue = this.$refs.goal.value;
+            if(enteredValue=== ''){
+                this.inputIsInvalid = true;
+            }
+        },
+        confirmError(){
+            this.inputIsInvalid = false;
+        }
+    }
+}
+</script>
