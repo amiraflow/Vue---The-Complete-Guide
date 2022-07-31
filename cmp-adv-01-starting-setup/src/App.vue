945262ff93596a7fe2e29cf79dba933a907b8e74
amiraflow <amira.midzic@gmail.com>
2022-07-31 12:00:01 +0200

    Dynamic components

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -12,23 +12,32 @@
         <h2> {{slotProps.item}}</h2>
       </template>
       
-    </course-goals>
+    </course-goals> -->
+
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals></active-goals>
+    <manage-goals></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+    
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
-    'the-header': TheHeader,
-    'badge-list': BadgeList,
-    'user-info': UserInfo,
-    'course-goals': CourseGoals,
-    CourseGoals,
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
     //or TheHeader: TheHeader, or TheHeader
   },
   data() {
@@ -38,9 +47,15 @@ export default {
         description: 'Site owner and admin',
         role: 'admin',
       },
+      selectedComponent: 'active-goals',
     };
   },
-};
+  methods: {
+    setSelectedComponent(cmp){
+      this.selectedComponent = cmp;
+    },
+  },
+  };
 </script>
 
 <style>
