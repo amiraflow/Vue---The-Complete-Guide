f0b9a1db9228edd68cb3d9c8e7cf34fcf7e30e61
amiraflow <amira.midzic@gmail.com>
2022-08-18 11:05:15 +0200

    Example of using state

---
+++ vuex-01-starting-setup/src/components/TheCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>{{ counter }}</h3>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  }
+};
+</script>
