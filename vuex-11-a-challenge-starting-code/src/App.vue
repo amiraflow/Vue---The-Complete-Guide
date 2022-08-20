7af5c16f480ea9b2289e791729e02438a78ed7d4
amiraflow <amira.midzic@gmail.com>
2022-08-20 20:58:06 +0200

    Using vuex for a simple shop

---
+++ vuex-11-a-challenge-starting-code/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <the-header></the-header>
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  },
+
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
