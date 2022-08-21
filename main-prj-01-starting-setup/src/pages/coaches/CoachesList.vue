7725af8c4330e27f0029612759cc357bb1c44644
amiraflow <amira.midzic@gmail.com>
2022-08-21 20:21:07 +0200

    Filtering coaches

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
 <section>
-    FILTER
+    <coach-filter @change-filter="setFilters"></coach-filter>
 </section>
 
 <section>
@@ -28,17 +28,45 @@
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
 
 export default {
     components: {
         CoachItem,
+        CoachFilter
+    },
+    data(){
+        return {
+            activeFilters: {
+                frontend: true,
+                backend: true,
+                career: true,
+            }
+        }
     },
     computed: {
         filteredCoaches(){
-            return this.$store.getters['coaches/coaches'];
+            const coaches = this.$store.getters['coaches/coaches'];
+            return coaches.filter(coach => {
+                if (this.activeFilters.frontend && coach.areas.includes('frontend')){
+                    return true;
+                }
+                if (this.activeFilters.backend && coach.areas.includes('backend')){
+                    return true;
+                }
+                if (this.activeFilters.career && coach.areas.includes('career')){
+                    return true;
+                }
+                return false;
+            })
         },
         hasCoaches(){
             return this.$store.getters['coaches/hasCoaches'];
+        }
+    },
+    methods: {
+        setFilters(updatedFilters){
+            this.activeFilters = updatedFilters;
         }
     }
 }
