761cc77a3b7b5f28af794e46262f672f6c65fa1e
amiraflow <amira.midzic@gmail.com>
2022-08-21 10:41:09 +0200

    Beginning: routing and components

---
+++ main-prj-01-starting-setup/src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/coaches' },
+        { path: '/coaches', component: CoachesList },
+        { path: '/coaches/:id', component: CoachDetail, children: [
+            { path: '/contact', component: ContactCoach }, //coaches/id/contact
+        ] },
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestsReceived },
+        { path: '/:notFound(.*)', component: NotFound },
+    ],
+});
+
+
+export default router;
