4c463c780ea427c3a4257ffa2ddd42d8e28a955e
amiraflow <amira.midzic@gmail.com>
2022-08-21 11:30:50 +0200

    Single Coach component

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -9,9 +9,12 @@
         <router-link to="/register">Register as Coach</router-link>    
         </div>
     <ul v-if="hasCoaches">
-        <li v-for="coach in filteredCoaches" :key="coach.id">
-            {{ coach.firstName }}
-        </li>
+        <coach-item v-for="coach in filteredCoaches" 
+        :key="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas" ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
 </section>
@@ -20,7 +23,12 @@
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+    components: {
+        CoachItem,
+    },
     computed: {
         filteredCoaches(){
             return this.$store.getters['coaches/coaches'];
@@ -31,3 +39,16 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
