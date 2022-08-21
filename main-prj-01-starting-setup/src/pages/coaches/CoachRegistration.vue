614b0968f1f221d9b380b7e32a28b59afc43442b
amiraflow <amira.midzic@gmail.com>
2022-08-22 01:35:49 +0200

    Registration of a coach, form and validation

--- main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
@@ -1,5 +1,25 @@
 <template>
-<div>
-    REGISTER
-</div>
+    <section>
+        <base-card>
+        <h2>Register as a coach now!</h2>
+        <coach-form @save-data="saveData"></coach-form>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+    components: {
+        CoachForm,
+    },
+    methods: {
+        saveData(data) {
+            this.$store.dispatch('coaches/registerCoach', data);
+            this.$router.replace('/coaches');
+        }
+    }
+}
+</script>
+
