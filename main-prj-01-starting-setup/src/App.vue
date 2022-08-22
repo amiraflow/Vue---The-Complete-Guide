837809f5a5520cca5435f6afb7cf6dae66fcbafd
amiraflow <amira.midzic@gmail.com>
2022-08-22 09:02:50 +0200

    Added NotFound and transitions

--- main-prj-01-starting-setup/src/App.vue
+++ main-prj-01-starting-setup/src/App.vue
@@ -1,7 +1,11 @@
 <template>
 <div>
 <the-header></the-header>
-<router-view></router-view>
+<router-view v-slot="slotProps">
+  <transition name="route" mode="out-in">
+    <component :is="slotProps.Component"></component>
+  </transition>
+</router-view>
 </div>
 </template>
 
@@ -29,5 +33,29 @@ html {
 
 body {
   margin: 0;
+}
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
--- main-prj-01-starting-setup/src/components/ui/BaseDialog.vue
+++ main-prj-01-starting-setup/src/components/ui/BaseDialog.vue
@@ -1,6 +1,7 @@
 <template>
   <teleport to="body">
     <div v-if="show" @click="tryClose" class="backdrop"></div>
+    <transition name="dialog">
     <dialog open v-if="show">
       <header>
         <slot name="header">
@@ -16,6 +17,7 @@
         </slot>
       </menu>
     </dialog>
+    </transition>
   </teleport>
 </template>
 
@@ -94,6 +96,24 @@ menu {
   display: flex;
   justify-content: flex-end;
   margin: 0;
+}
+
+.dialog-enter-from, .dialog-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.dialog-enter-active {
+  transition: al 0.3s ease-out;
+}
+
+.dialog-leave-active {
+  transition: al 0.3s ease-in;
+}
+
+.dialog-enter-to, .dialog-leave-from {
+  opacity: 1;
+  transform: scale(1);
 }
 
 @media (min-width: 768px) {
